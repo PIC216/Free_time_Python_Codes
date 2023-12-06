b8a2ce36ae821cf2b296840a9a6f870d6df25dea
Phoebe <101755626+PIC216@users.noreply.github.com>
2023-12-06 08:28:37 +0000

    Update power_of_a_number.py

--- Mathematical Codes/power_of_a_number.py
+++ Mathematical Codes/power_of_a_number.py
@@ -1,35 +1,33 @@
 '''
-If you can't use the ** operator or
-the power function, how can you find
-the power of a number?
+Problem: If you can't use the ** operator or the power function, how can you find the power of a number?
 
-This code takes two inputs, number and
-power and returns a sentence with the
-answer.
+This code takes two inputs, number and power, and returns a sentence with the answer.
 
-Note: this code does have it's
-limitations, it is souly focusing
-on the regular cases of calculating
-a positive integer to the power of
-a positive integer.
+Note: this code does have it's limitations, it is souly focusing on the regular cases of calculating
+values to the power of a positive integer (or 0).
 '''
 
-print("""Please give a number and a
-         power. To end the calculations
-         press 0 for either number
-         or power.""")
+print("""Please give a number and a power. To end the calculations press 0 for number.""")
 
 while True:
-  number = int(input("Number: ")
-  if number == 0:
-    break
-  power = int(input("Power: "))
-  if power == 0:
-    break
-  counter = power
-  value = number
-  while counter > 1:
-    value *= number
-    counter -= 1
-  print(f"""{number} to the power of
-            {power} is {value}.""")
+    number = float(input("Number: "))
+    if number == 0:
+        break
+    power = float(input("Power: "))
+    if power == 0:
+        value = 1
+    elif power == int(power) and power > 0:
+        power = int(power)
+        value = 1
+        for i in range(power):
+            value = value*number
+    else:
+        print("Not an acceptable value for power")
+        continue
+             
+    if number == int(number):
+        number = int(number)
+    if value == int(value):
+        value = int(value)
+             
+    print(f"""{number} to the power of {power} is {value}.""")
