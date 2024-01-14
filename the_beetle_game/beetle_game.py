06430148c0aa5b2fd9216e1d2a30e1f1b9cbc659
Phoebe <101755626+PIC216@users.noreply.github.com>
2024-01-14 22:42:58 +0000

    Create beetle_game.py

---
+++ the_beetle_game/beetle_game.py
@@ -0,0 +1,10 @@
+"""
+File to play the beetle game from beetle_game_classes.py
+Beetle game: https://en.wikipedia.org/wiki/Beetle_(game)
+"""
+
+from beetle_game_classes import BeetleGame
+
+beetle_game = BeetleGame()
+beetle_game.initialise_game()
+beetle_game.play_game()
